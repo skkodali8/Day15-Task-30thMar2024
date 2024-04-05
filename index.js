66bdee26031e15563a98243bf79a64b48ddb34a5
skkodali8 <157340642+skkodali8@users.noreply.github.com>
2024-04-05 16:40:11 +0530

    Files Added

---
+++ index.js
@@ -0,0 +1,23 @@
+
+
+
+        function countdown() {
+            let count = 10;
+            const countdownContainer = document.getElementById('countdownContainer');
+
+            function displayCount() {
+                countdownContainer.innerText = count;
+                count--;
+
+                if (count >= 0) {
+                    setTimeout(displayCount, 1000);
+                } else {
+                    countdownContainer.innerText = 'HappyIndepence Day';
+                }
+            }
+
+            displayCount();
+        }
+
+        countdown();
+    
